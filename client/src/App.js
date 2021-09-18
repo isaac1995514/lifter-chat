3644e9ff1fc1e20e39d1b914aaab9ed595556dd1
Isaac Leong <isaacleong@microstrategy.com>
2021-09-18 13:50:31 -0400

    App Template

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+
+import Cookies from "universal-cookie";
+
+import { STREAM_API_KEY } from "./.secret/api-key";
+
+import "./App.css";
+
+const client = StreamChat.getInstance(STREAM_API_KEY);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team dark">
+        <ChannelList></ChannelList>
+
+        <ChannelRoom></ChannelRoom>
+      </Chat>
     </div>
   );
 }
